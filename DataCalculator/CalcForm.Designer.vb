ddd8e3a53371bccbffc44e9fdd2527d2ab1dd4c8
Umaroth <mkramer071409@gmail.com>
2014-07-16 07:15:17 -0400

    Version 1.0

---
+++ DataCalculator/CalcForm.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDataCalc
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmDataCalc))
+        Me.lblFromType = New System.Windows.Forms.Label()
+        Me.lblToType = New System.Windows.Forms.Label()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.comboToType = New System.Windows.Forms.ComboBox()
+        Me.comboFromType = New System.Windows.Forms.ComboBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.lblInput = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.ResultBox = New System.Windows.Forms.TextBox()
+        Me.pnlDataCalc = New System.Windows.Forms.Panel()
+        Me.rdoData = New System.Windows.Forms.RadioButton()
+        Me.rdoText = New System.Windows.Forms.RadioButton()
+        Me.pnlRadios = New System.Windows.Forms.Panel()
+        Me.pnlDataCalc.SuspendLayout()
+        Me.pnlRadios.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblFromType
+        '
+        Me.lblFromType.AutoSize = True
+        Me.lblFromType.Location = New System.Drawing.Point(12, 7)
+        Me.lblFromType.Margin = New System.Windows.Forms.Padding(0)
+        Me.lblFromType.Name = "lblFromType"
+        Me.lblFromType.Size = New System.Drawing.Size(99, 16)
+        Me.lblFromType.TabIndex = 99
+        Me.lblFromType.Text = "From Data Type"
+        '
+        'lblToType
+        '
+        Me.lblToType.AutoSize = True
+        Me.lblToType.Location = New System.Drawing.Point(153, 7)
+        Me.lblToType.Margin = New System.Windows.Forms.Padding(0)
+        Me.lblToType.Name = "lblToType"
+        Me.lblToType.Size = New System.Drawing.Size(82, 16)
+        Me.lblToType.TabIndex = 99
+        Me.lblToType.Text = "To Data Type"
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Font = New System.Drawing.Font("Modern No. 20", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConvert.ForeColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.btnConvert.Location = New System.Drawing.Point(83, 93)
+        Me.btnConvert.Margin = New System.Windows.Forms.Padding(0)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.btnConvert.Size = New System.Drawing.Size(75, 38)
+        Me.btnConvert.TabIndex = 4
+        Me.btnConvert.Text = "Convert!"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'comboToType
+        '
+        Me.comboToType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.comboToType.FormattingEnabled = True
+        Me.comboToType.Items.AddRange(New Object() {"Binary", "Octal", "Decimal", "Hexadecimal"})
+        Me.comboToType.Location = New System.Drawing.Point(133, 23)
+        Me.comboToType.Margin = New System.Windows.Forms.Padding(0)
+        Me.comboToType.Name = "comboToType"
+        Me.comboToType.Size = New System.Drawing.Size(121, 24)
+        Me.comboToType.TabIndex = 2
+        '
+        'comboFromType
+        '
+        Me.comboFromType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.comboFromType.FormattingEnabled = True
+        Me.comboFromType.Items.AddRange(New Object() {"Binary", "Octal", "Decimal", "Hexadecimal"})
+        Me.comboFromType.Location = New System.Drawing.Point(0, 23)
+        Me.comboFromType.Margin = New System.Windows.Forms.Padding(0)
+        Me.comboFromType.Name = "comboFromType"
+        Me.comboFromType.Size = New System.Drawing.Size(121, 24)
+        Me.comboFromType.TabIndex = 1
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(99, 143)
+        Me.lblResult.Margin = New System.Windows.Forms.Padding(0)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(49, 16)
+        Me.lblResult.TabIndex = 99
+        Me.lblResult.Text = "Result!"
+        '
+        'lblInput
+        '
+        Me.lblInput.AutoSize = True
+        Me.lblInput.Location = New System.Drawing.Point(108, 51)
+        Me.lblInput.Name = "lblInput"
+        Me.lblInput.Size = New System.Drawing.Size(37, 16)
+        Me.lblInput.TabIndex = 99
+        Me.lblInput.Text = "Input"
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(43, 67)
+        Me.txtInput.Margin = New System.Windows.Forms.Padding(0)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(156, 23)
+        Me.txtInput.TabIndex = 3
+        '
+        'ResultBox
+        '
+        Me.ResultBox.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.ResultBox.Location = New System.Drawing.Point(43, 158)
+        Me.ResultBox.Margin = New System.Windows.Forms.Padding(0)
+        Me.ResultBox.Name = "ResultBox"
+        Me.ResultBox.ReadOnly = True
+        Me.ResultBox.Size = New System.Drawing.Size(156, 23)
+        Me.ResultBox.TabIndex = 5
+        Me.ResultBox.TabStop = False
+        '
+        'pnlDataCalc
+        '
+        Me.pnlDataCalc.Controls.Add(Me.comboFromType)
+        Me.pnlDataCalc.Controls.Add(Me.comboToType)
+        Me.pnlDataCalc.Controls.Add(Me.ResultBox)
+        Me.pnlDataCalc.Controls.Add(Me.lblResult)
+        Me.pnlDataCalc.Controls.Add(Me.lblFromType)
+        Me.pnlDataCalc.Controls.Add(Me.btnConvert)
+        Me.pnlDataCalc.Controls.Add(Me.txtInput)
+        Me.pnlDataCalc.Controls.Add(Me.lblInput)
+        Me.pnlDataCalc.Controls.Add(Me.lblToType)
+        Me.pnlDataCalc.Location = New System.Drawing.Point(9, 43)
+        Me.pnlDataCalc.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlDataCalc.Name = "pnlDataCalc"
+        Me.pnlDataCalc.Size = New System.Drawing.Size(255, 241)
+        Me.pnlDataCalc.TabIndex = 101
+        '
+        'rdoData
+        '
+        Me.rdoData.AutoSize = True
+        Me.rdoData.Location = New System.Drawing.Point(3, 7)
+        Me.rdoData.Name = "rdoData"
+        Me.rdoData.Size = New System.Drawing.Size(115, 20)
+        Me.rdoData.TabIndex = 103
+        Me.rdoData.TabStop = True
+        Me.rdoData.Text = "Data Calculator"
+        Me.rdoData.UseVisualStyleBackColor = True
+        '
+        'rdoText
+        '
+        Me.rdoText.AutoSize = True
+        Me.rdoText.Location = New System.Drawing.Point(113, 9)
+        Me.rdoText.Name = "rdoText"
+        Me.rdoText.Size = New System.Drawing.Size(109, 20)
+        Me.rdoText.TabIndex = 103
+        Me.rdoText.TabStop = True
+        Me.rdoText.Text = "Text Converter"
+        Me.rdoText.UseVisualStyleBackColor = True
+        '
+        'pnlRadios
+        '
+        Me.pnlRadios.Controls.Add(Me.rdoText)
+        Me.pnlRadios.Controls.Add(Me.rdoData)
+        Me.pnlRadios.Location = New System.Drawing.Point(29, 4)
+        Me.pnlRadios.Name = "pnlRadios"
+        Me.pnlRadios.Size = New System.Drawing.Size(221, 36)
+        Me.pnlRadios.TabIndex = 102
+        '
+        'frmDataCalc
+        '
+        Me.AcceptButton = Me.btnConvert
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(274, 257)
+        Me.Controls.Add(Me.pnlRadios)
+        Me.Controls.Add(Me.pnlDataCalc)
+        Me.Font = New System.Drawing.Font("Razer Text Regular", 9.749999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.SystemColors.Control
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmDataCalc"
+        Me.Text = "Data Calculator"
+        Me.pnlDataCalc.ResumeLayout(False)
+        Me.pnlDataCalc.PerformLayout()
+        Me.pnlRadios.ResumeLayout(False)
+        Me.pnlRadios.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lblFromType As System.Windows.Forms.Label
+    Friend WithEvents lblToType As System.Windows.Forms.Label
+    Friend WithEvents btnConvert As System.Windows.Forms.Button
+    Friend WithEvents comboToType As System.Windows.Forms.ComboBox
+    Friend WithEvents comboFromType As System.Windows.Forms.ComboBox
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents lblInput As System.Windows.Forms.Label
+    Friend WithEvents txtInput As System.Windows.Forms.TextBox
+    Friend WithEvents ResultBox As System.Windows.Forms.TextBox
+    Friend WithEvents pnlDataCalc As System.Windows.Forms.Panel
+    Friend WithEvents rdoData As System.Windows.Forms.RadioButton
+    Friend WithEvents rdoText As System.Windows.Forms.RadioButton
+    Friend WithEvents pnlRadios As System.Windows.Forms.Panel
+
+End Class
---
+++ DataCalculator/CalcForm.vb
@@ -0,0 +1,240 @@
+﻿Imports System.Runtime.InteropServices
+Module modVariables
+    Public fromTypeBase As Integer
+    Public toTypeBase As Integer
+    Public fromType As String
+    Public toType As String
+    Public validInputs() As String
+End Module
+
+Public Class frmDataCalc
+    Private Sub Form_Loaded(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        'Preselect the Data Calculator Radio button
+        rdoData.Checked = True
+        'Radio Panel Configuration
+        rdoData.Location = New Point(0, 0)
+        rdoText.Location = New Point(rdoData.Width + 5, 0)
+        pnlRadios.Size = New Size(rdoData.Width + rdoText.Width + 5, rdoData.Height)
+        'Center Objects
+        CenterPanels()
+        lblInput.Location = New Point((pnlDataCalc.Width - lblInput.Width) / 2, 50)
+        txtInput.Location = New Point((pnlDataCalc.Width - txtInput.Width) / 2, 65)
+        btnConvert.Location = New Point((pnlDataCalc.Width - btnConvert.Width) / 2, 95)
+        lblResult.Location = New Point((pnlDataCalc.Width - lblResult.Width) / 2, 150)
+        ResultBox.Location = New Point((pnlDataCalc.Width - ResultBox.Width) / 2, 165)
+    End Sub
+    Private Sub CenterPanels()
+        pnlRadios.Location = New Point((Me.DisplayRectangle.Width - pnlRadios.Width) / 2, 5)
+        pnlDataCalc.Location = New Point((Me.DisplayRectangle.Width - pnlDataCalc.Width) / 2, 40)
+    End Sub
+    Private Sub Form_Resized(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Resize
+        CenterPanels()
+    End Sub
+    Private Sub ResultBox_GotFocus(sender As Object, e As EventArgs) Handles ResultBox.GotFocus
+        HideCaret(ResultBox.Handle)
+    End Sub
+    <DllImport("user32.dll")> _
+    Private Shared Function HideCaret(hWnd As IntPtr) As Boolean
+    End Function
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        If toType = fromType Then
+            ResultBox.Text = "INVALID SELECTION"
+        ElseIf Not isValidInput(txtInput.Text) Then
+            ResultBox.Text = "INVALID INPUT"
+        ElseIf toType = fromType Then
+            ResultBox.Text = "INVALID SELECTION"
+        ElseIf fromType = "Decimal" Then
+            ResultBox.Text = ConvertFromDecimal(txtInput.Text)
+        ElseIf toType = "Decimal" Then
+            ResultBox.Text = ConvertToDecimal(txtInput.Text)
+        Else
+            ResultBox.Text = ConvertFromDecimal(ConvertToDecimal(txtInput.Text))
+        End If
+    End Sub
+    Private Sub comboToType_SelectedIndexChanged(sender As Object, e As EventArgs) Handles comboToType.SelectedIndexChanged
+        Select Case comboToType.SelectedItem
+            Case "Binary"
+                toType = "Binary"
+                toTypeBase = 2
+            Case "Octal"
+                toType = "Octal"
+                toTypeBase = 8
+            Case "Decimal"
+                toType = "Decimal"
+                toTypeBase = 10
+            Case "Hexadecimal"
+                toType = "Hexadecimal"
+                toTypeBase = 16
+        End Select
+    End Sub
+    Private Sub comboFromType_SelectedIndexChanged(sender As Object, e As EventArgs) Handles comboFromType.SelectedIndexChanged
+        Select Case comboFromType.SelectedItem
+            Case "Binary"
+                fromTypeBase = 2
+                fromType = "Binary"
+                validInputs = {"0", "1"}
+            Case "Octal"
+                fromTypeBase = 8
+                fromType = "Octal"
+                validInputs = {"0", "1", "2", "3", "4", "5", "6", "7"}
+            Case "Decimal"
+                fromTypeBase = 10
+                fromType = "Decimal"
+                validInputs = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+            Case "Hexadecimal"
+                fromTypeBase = 16
+                fromType = "Hexadecimal"
+                validInputs = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F"}
+        End Select
+    End Sub
+    Private Sub rdoData_CheckedChanged(sender As Object, e As EventArgs) Handles rdoData.CheckedChanged
+
+    End Sub
+    Private Sub rdoText_CheckedChanged(sender As Object, e As EventArgs) Handles rdoText.CheckedChanged
+
+    End Sub
+End Class
+Module modFunctions
+    Function isValidInput(ByVal strInput As String) As Boolean
+
+        For Each e In strInput
+            If validInputs.Contains(e) Then
+            Else
+                Return False
+            End If
+        Next
+        Return True
+    End Function
+    Function ConvertStringtoNumber(ByVal strInput As String) As Array
+        Dim arrArrayofInputValues(strInput.Length - 1) As Long
+        Dim Counter As Integer = 0
+
+        For Each e As Char In strInput.ToLower
+            Select Case e
+                Case "0"
+                    arrArrayofInputValues(Counter) = 0
+                Case "1"
+                    arrArrayofInputValues(Counter) = 1
+                Case "2"
+                    arrArrayofInputValues(Counter) = 2
+                Case "3"
+                    arrArrayofInputValues(Counter) = 3
+                Case "4"
+                    arrArrayofInputValues(Counter) = 4
+                Case "5"
+                    arrArrayofInputValues(Counter) = 5
+                Case "6"
+                    arrArrayofInputValues(Counter) = 6
+                Case "7"
+                    arrArrayofInputValues(Counter) = 7
+                Case "8"
+                    arrArrayofInputValues(Counter) = 8
+                Case "9"
+                    arrArrayofInputValues(Counter) = 9
+                Case "a"
+                    arrArrayofInputValues(Counter) = 10
+                Case "b"
+                    arrArrayofInputValues(Counter) = 11
+                Case "c"
+                    arrArrayofInputValues(Counter) = 12
+                Case "d"
+                    arrArrayofInputValues(Counter) = 13
+                Case "e"
+                    arrArrayofInputValues(Counter) = 14
+                Case "f"
+                    arrArrayofInputValues(Counter) = 15
+            End Select
+            Counter = Counter + 1
+        Next
+
+        Return arrArrayofInputValues
+    End Function
+    Function ConvertNumberToString(ByVal intInput As Array) As String
+        Dim arrArrayofOutputChars(intInput.Length - 1) As Char
+        Dim Counter As Integer = 0
+
+        For Each e In intInput
+            Select Case e
+                Case "0"
+                    arrArrayofOutputChars(Counter) = "0"
+                Case "1"
+                    arrArrayofOutputChars(Counter) = "1"
+                Case "2"
+                    arrArrayofOutputChars(Counter) = "2"
+                Case "3"
+                    arrArrayofOutputChars(Counter) = "3"
+                Case "4"
+                    arrArrayofOutputChars(Counter) = "4"
+                Case "5"
+                    arrArrayofOutputChars(Counter) = "5"
+                Case "6"
+                    arrArrayofOutputChars(Counter) = "6"
+                Case "7"
+                    arrArrayofOutputChars(Counter) = "7"
+                Case "8"
+                    arrArrayofOutputChars(Counter) = "8"
+                Case "9"
+                    arrArrayofOutputChars(Counter) = "9"
+                Case "10"
+                    arrArrayofOutputChars(Counter) = "A"
+                Case "11"
+                    arrArrayofOutputChars(Counter) = "B"
+                Case "12"
+                    arrArrayofOutputChars(Counter) = "C"
+                Case "13"
+                    arrArrayofOutputChars(Counter) = "D"
+                Case "14"
+                    arrArrayofOutputChars(Counter) = "E"
+                Case "15"
+                    arrArrayofOutputChars(Counter) = "F"
+            End Select
+            Counter = Counter + 1
+        Next
+
+        Return arrArrayofOutputChars
+    End Function
+    Function ConvertToDecimal(ByVal strInput As String) As String
+        Dim arrArrayOfInputValues() As Long = ConvertStringtoNumber(strInput)
+        Dim ReverseCounter As Integer = arrArrayOfInputValues.Length
+        Dim Result As Long
+
+        For Each e As Integer In arrArrayOfInputValues
+            ReverseCounter = ReverseCounter - 1
+            Result = Result + (e * fromTypeBase ^ ReverseCounter)
+        Next
+
+        Return Result
+    End Function
+    Function ConvertFromDecimal(ByVal intInput As Long) As String
+
+        Dim Remainder As Long
+        Dim intLooper As Decimal
+        Dim intLooper2 As Long = intInput
+        Dim wholePart As Long
+        Dim Counter As Integer = 0
+        Dim digitCount As Integer = 0
+
+        Do
+            intLooper = intLooper2 / toTypeBase
+            intLooper = Math.Truncate(intLooper)
+            intLooper2 = intLooper
+            digitCount = digitCount + 1
+        Loop While intLooper > 0
+
+        Dim arrArrayOfValues(digitCount - 1) As Integer
+
+        Do
+            intLooper = intInput / toTypeBase
+            wholePart = Math.Truncate(intLooper)
+            Remainder = intInput - (wholePart * toTypeBase)
+            intInput = wholePart
+            arrArrayOfValues(Counter) = Remainder
+            Counter = Counter + 1
+        Loop While wholePart > 0
+
+        Array.Reverse(arrArrayOfValues)
+
+        Dim Result As String = ConvertNumberToString(arrArrayOfValues)
+        Return Result
+    End Function
+End Module
